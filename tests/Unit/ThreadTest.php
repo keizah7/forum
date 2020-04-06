8450ee03d246cf75d473b465feb4b0c0813e65cf
keizah7 <murashka.arturas@gmail.com>
2020-04-06 17:50:08 +0300

    Notification Fakes in a Nutshell

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -3,10 +3,12 @@
 namespace Tests\Unit;
 
 use App\Channel;
+use App\Notifications\ThreadWasUpdated;
 use App\Reply;
 use App\Thread;
 use App\User;
 use Illuminate\Database\Eloquent\Collection;
+use Notification;
 use Tests\TestCase;
 
 class ThreadTest extends TestCase
@@ -51,6 +53,21 @@ class ThreadTest extends TestCase
         ]);
 
         $this->assertCount(1, $this->thread->replies);
+    }
+
+    /** @test */
+    function a_thread_notifies_all_registered_subscribers_when_a_reply_is_added()
+    {
+        Notification::fake();
+        $this->signIn()
+            ->thread
+            ->subscribe()
+            ->addReply([
+                'body' => 'Foobar',
+                'user_id' => create(User::class)->id
+            ]);
+
+        Notification::assertSentTo(auth()->user(), ThreadWasUpdated::class);
     }
 
     /** @test */
