2f5dfff3bcbf35955a7e3a2fc2f8729a13dcf214
keizah7 <murashka.arturas@gmail.com>
2020-04-16 08:20:20 +0300

    First Class Search: Faceting and Ranking

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -9,6 +9,24 @@
                 {{ $threads->links() }}
             </div>
             <div class="col-md-4">
+                <div class="card mb-2">
+                    <div class="card-header">
+                        Search
+                    </div>
+
+                    <div class="card-body">
+                        <form method="GET" action="/threads/search">
+                            <div class="form-group">
+                                <input type="text" placeholder="Search for something..." name="q" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <button class="btn btn-primary" type="submit">Search</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
                 @if (count($trending))
                     <div class="card">
                         <div class="card-header">
