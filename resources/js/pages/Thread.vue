c26a783986f466b547ff8ba12091f1c94b85ee69
keizah7 <murashka.arturas@gmail.com>
2020-04-16 07:15:16 +0300

    A Thread Can Be Updated: Part 3

--- resources/js/pages/Thread.vue
+++ resources/js/pages/Thread.vue
@@ -19,5 +19,28 @@
             this.resetForm();
         },
         components: {Replies, SubscribeButton},
+        methods: {
+            toggleLock() {
+                let uri = `/locked-threads/${this.thread.slug}`;
+                axios[this.locked ? 'delete' : 'post'](uri);
+                this.locked = !this.locked;
+            },
+            update() {
+                let uri = `/threads/${this.thread.channel.slug}/${this.thread.slug}`;
+                axios.patch(uri, this.form).then(() => {
+                    this.editing = false;
+                    this.title = this.form.title;
+                    this.body = this.form.body;
+                    flash('Your thread has been updated.');
+                })
+            },
+            resetForm() {
+                this.form = {
+                    title: this.title,
+                    body: this.body,
+                };
+                this.editing = false;
+            }
+        }
     }
 </script>
--- resources/views/threads/_question.blade.php
+++ resources/views/threads/_question.blade.php
@@ -1,22 +1,22 @@
 <div class="card mb-3" v-if="editing">
     <div class="card-header d-flex justify-content-between items-center">
-        <input class="form-control mr-2" type="text" value="{{ $thread->title }}">
+        <input class="form-control mr-2" type="text" v-model="form.title">
     </div>
 
     <div class="card-body">
         <article class="form-group">
-            <textarea class="form-control" id="" cols="30" rows="5">{{ $thread->body }}</textarea>
+            <textarea class="form-control" id="" cols="30" rows="5" v-model="form.body"></textarea>
         </article>
     </div>
 
     <div class="card-footer d-flex justify-content-between">
         <div>
-        <button class="btn btn-sm btn-primary mr-1" @click="update">Update</button>
-        <button class="btn btn-sm" @click="resetForm">Cancel</button>
+            <button class="btn btn-sm btn-primary mr-1" @click="update">Update</button>
+            <button class="btn btn-sm" @click="resetForm">Cancel</button>
         </div>
 
 
-    @can('update', $thread)
+        @can('update', $thread)
             <form action="/threads/{{ $thread->id }}" method="post">
                 @csrf
                 @method('delete')
@@ -34,18 +34,18 @@
                  width="25"
                  height="25"
                  class="mr-1">
-            <a class="mr-2"
-               href="{{ route('user.profile', $thread->creator) }}">{{ $thread->creator->name }}</a> {{ $thread->title }}
+            <a class="mr-2" href="{{ route('user.profile', $thread->creator) }}">{{ $thread->creator->name }}</a>
+            <span v-text="title"></span>
         </div>
     </div>
 
     <div class="card-body">
         <article>
-            <p>{{ $thread->body }}</p>
+            <p v-text="body"></p>
         </article>
     </div>
 
-    <div class="card-footer">
+    <div class="card-footer" v-if="authorize('owns', thread)">
         <button class="btn btn-sm btn-success mr-1" @click="editing = true" v-show="! editing">Edit</button>
     </div>
 </div>
